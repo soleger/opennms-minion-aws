9bf89972682b86c971d14ff1335832e1250e86e5
Jesse White <jesse@opennms.org>
2016-12-12 21:05:07 -0500

    Misc. updates.

--- files/start-udpgen.sh
+++ files/start-udpgen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 {% for host in groups['tag_MinionLabInstanceType_loadbalancer'] %}
-udpgen -d -h {{ hostvars[host]['ec2_private_ip_address'] }} -p 1514
+udpgen -d -h {{ hostvars[host]['ec2_private_ip_address'] }} -p 1514 -t 2
 {% endfor %}
